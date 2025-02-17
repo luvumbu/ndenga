02f5af5495070a275f4831d605a8bf60c8b7c953
luvumbu <luvumbu.n@gmail.com>
2025-02-17 14:41:04 +0100

    mode HTML on ou off ok

--- view/div_form.php
+++ view/div_form.php
@@ -62,9 +62,21 @@
 
  <div title="<?php echo $id_sha1_projet_dynamic_1_; ?>" onkeyup="all_blok(this)" contenteditable="true" class="blog-content_1" id="title_projet_<?php echo $id_sha1_projet_dynamic_1_; ?>">
      <?php
-        echo AsciiConverter::asciiToString($title_projet_dynamic_1_); // Affiche "Hello"
+
+        if ($html_mode_projet_1_dynamic_1_ != "on") {
+
+            echo AsciiConverter::asciiToString($title_projet_dynamic_1_); // Affiche "Hello"
+
+        } else {
+            $texteNettoye = nettoyerTexteHtml(AsciiConverter::asciiToString($title_projet_dynamic_1_));
+
+            echo $texteNettoye;
+        }
+
         ?>
  </div>
+
+
 
  <?php
 
@@ -91,29 +103,42 @@
     } else {
     ?>
      <div class="display_flex_1">
-     <div onclick="html_mode_projet_dynamic_1(this)" class="<?php echo $id_sha1_projet_dynamic_1_ ?>">
-         <img width="50" height="50" src="https://img.icons8.com/ios/50/html.png" alt="delete-forever--v1" />
-         <br />
-         <img id="<?php echo 'html_1_' . $id_sha1_projet_dynamic_1_ ?>" width="20" height="20" src="https://img.icons8.com/material-sharp/20/switch-on.png" alt="switch-on" />
-         <img id="<?php echo 'html_2_' . $id_sha1_projet_dynamic_1_ ?>" style="opacity: 0.2;" width="20" height="20" src="https://img.icons8.com/ios/20/switch-off--v1.png" alt="switch-off--v1" />
+         <div onclick="html_mode_projet_dynamic_1(this)" class="<?php echo $id_sha1_projet_dynamic_1_ ?>">
+             <img width="50" height="50" src="https://img.icons8.com/ios/50/html.png" alt="delete-forever--v1" />
+             <br />
+             <img id="<?php echo 'html_1_' . $id_sha1_projet_dynamic_1_ ?>" width="20" height="20" src="https://img.icons8.com/material-sharp/20/switch-on.png" alt="switch-on" />
+             <img id="<?php echo 'html_2_' . $id_sha1_projet_dynamic_1_ ?>" style="opacity: 0.2;" width="20" height="20" src="https://img.icons8.com/ios/20/switch-off--v1.png" alt="switch-off--v1" />
 
-     </div>
+         </div>
          <div>
              <div class="margin_top_50" onclick="red_box_1(this)" title="<?php echo $id_sha1_projet_dynamic_1_; ?>"><img width="50" height="50" src="https://img.icons8.com/material-sharp/50/clear-symbol--v1.png" alt="clear-symbol--v1" /></div>
 
          </div>
 
      </div>
- 
+
  <?php
     }
 
     ?>
 
  <div title="<?php echo $id_sha1_projet_dynamic_1_; ?>" onkeyup="all_blok(this)" contenteditable="true" class="blog-content_2" id="description_projet_<?php echo $id_sha1_projet_dynamic_1_; ?>">
+
+
+
      <?php
-        echo AsciiConverter::asciiToString($description_projet_dynamic_1_); // Affiche "Hello"
+
+        if ($html_mode_projet_2_dynamic_1_ != "on") {
+
+            echo AsciiConverter::asciiToString($description_projet_dynamic_1_); // Affiche "Hello"
+
+
+        } else {
+            $texteNettoye = nettoyerTexteHtml(AsciiConverter::asciiToString($description_projet_dynamic_1_));
+            echo $texteNettoye;
+        }
         ?>
+
  </div>
 
 
@@ -122,47 +147,47 @@
 
 
 
-if ($html_mode_projet_2_dynamic_1_ != "on") {
-?>
+    if ($html_mode_projet_2_dynamic_1_ != "on") {
+    ?>
 
 
 
- <div class="display_flex_1">
-     <div onclick="html_mode_projet_dynamic_2(this)" class="<?php echo $id_sha1_projet_dynamic_1_ ?>">
-         <img width="50" height="50" src="https://img.icons8.com/ios/50/html.png" alt="delete-forever--v1" />
-         <br />
-         <img id="<?php echo 'html_11_' . $id_sha1_projet_dynamic_1_ ?>" style="opacity: 0.2;" width="20" height="20" src="https://img.icons8.com/material-sharp/20/switch-on.png" alt="switch-on" />
-         <img id="<?php echo 'html_22_' . $id_sha1_projet_dynamic_1_ ?>" width="20" height="20" src="https://img.icons8.com/ios/20/switch-off--v1.png" alt="switch-off--v1" />
+     <div class="display_flex_1">
+         <div onclick="html_mode_projet_dynamic_2(this)" class="<?php echo $id_sha1_projet_dynamic_1_ ?>">
+             <img width="50" height="50" src="https://img.icons8.com/ios/50/html.png" alt="delete-forever--v1" />
+             <br />
+             <img id="<?php echo 'html_11_' . $id_sha1_projet_dynamic_1_ ?>" style="opacity: 0.2;" width="20" height="20" src="https://img.icons8.com/material-sharp/20/switch-on.png" alt="switch-on" />
+             <img id="<?php echo 'html_22_' . $id_sha1_projet_dynamic_1_ ?>" width="20" height="20" src="https://img.icons8.com/ios/20/switch-off--v1.png" alt="switch-off--v1" />
 
-     </div>
-     <div>
-         <div class="margin_top_50" onclick="red_box_1(this)" title="<?php echo $id_sha1_projet_dynamic_1_; ?>"><img width="50" height="50" src="https://img.icons8.com/material-sharp/50/clear-symbol--v1.png" alt="clear-symbol--v1" /></div>
+         </div>
+         <div>
+             <div class="margin_top_50" onclick="red_box_1(this)" title="<?php echo $id_sha1_projet_dynamic_1_; ?>"><img width="50" height="50" src="https://img.icons8.com/material-sharp/50/clear-symbol--v1.png" alt="clear-symbol--v1" /></div>
 
-     </div>
-
- </div>
-<?php
-} else {
-?>
- <div class="display_flex_1">
- <div onclick="html_mode_projet_dynamic_2(this)" class="<?php echo $id_sha1_projet_dynamic_1_ ?>">
-     <img width="50" height="50" src="https://img.icons8.com/ios/50/html.png" alt="delete-forever--v1" />
-     <br />
-     <img id="<?php echo 'html_11_' . $id_sha1_projet_dynamic_1_ ?>" width="20" height="20" src="https://img.icons8.com/material-sharp/20/switch-on.png" alt="switch-on" />
-     <img id="<?php echo 'html_22_' . $id_sha1_projet_dynamic_1_ ?>" style="opacity: 0.2;" width="20" height="20" src="https://img.icons8.com/ios/20/switch-off--v1.png" alt="switch-off--v1" />
-
- </div>
-     <div>
-         <div class="margin_top_50" onclick="red_box_2(this)" title="<?php echo $id_sha1_projet_dynamic_1_; ?>"><img width="50" height="50" src="https://img.icons8.com/material-sharp/50/clear-symbol--v1.png" alt="clear-symbol--v1" /></div>
+         </div>
 
      </div>
+ <?php
+    } else {
+    ?>
+     <div class="display_flex_1">
+         <div onclick="html_mode_projet_dynamic_2(this)" class="<?php echo $id_sha1_projet_dynamic_1_ ?>">
+             <img width="50" height="50" src="https://img.icons8.com/ios/50/html.png" alt="delete-forever--v1" />
+             <br />
+             <img id="<?php echo 'html_11_' . $id_sha1_projet_dynamic_1_ ?>" width="20" height="20" src="https://img.icons8.com/material-sharp/20/switch-on.png" alt="switch-on" />
+             <img id="<?php echo 'html_22_' . $id_sha1_projet_dynamic_1_ ?>" style="opacity: 0.2;" width="20" height="20" src="https://img.icons8.com/ios/20/switch-off--v1.png" alt="switch-off--v1" />
 
- </div>
+         </div>
+         <div>
+             <div class="margin_top_50" onclick="red_box_2(this)" title="<?php echo $id_sha1_projet_dynamic_1_; ?>"><img width="50" height="50" src="https://img.icons8.com/material-sharp/50/clear-symbol--v1.png" alt="clear-symbol--v1" /></div>
 
-<?php
-}
+         </div>
 
-?>
+     </div>
+
+ <?php
+    }
+
+    ?>
 
 
  <div id="blog-content-2<?php echo $id_sha1_projet_dynamic_1_; ?>" title="<?php echo $id_sha1_projet_dynamic_1_; ?>">
@@ -170,7 +195,7 @@ if ($html_mode_projet_2_dynamic_1_ != "on") {
  </div>
 
 
- 
+
 
 
 
@@ -276,56 +301,54 @@ if ($html_mode_projet_2_dynamic_1_ != "on") {
 
 
      function html_mode_projet_dynamic_2(_this) {
- 
-
-        var element = afficherValeursFormattees2(_this.className, " ");
 
 
+         var element = afficherValeursFormattees2(_this.className, " ");
 
 
 
 
 
-var opacite = document.getElementById("html_11_" + element[0]).style.opacity;
-
- console.log(opacite);
 
 
+         var opacite = document.getElementById("html_11_" + element[0]).style.opacity;
 
-
- if (opacite == 0.2) {
-
-   html_mode_projet = "on";
-   document.getElementById("html_11_" + element[0]).style.opacity = 1;
-   document.getElementById("html_22_" + element[0]).style.opacity = 0.2;
-
- } else {
-   document.getElementById("html_11_" + element[0]).style.opacity = 0.2;
-   document.getElementById("html_22_" + element[0]).style.opacity = 1;
-   html_mode_projet = "";
-
- }
+         console.log(opacite);
 
 
 
 
- var ok = new Information("config/html_mode_projet_2.php"); // création de la classe 
+         if (opacite == 0.2) {
+
+             html_mode_projet = "on";
+             document.getElementById("html_11_" + element[0]).style.opacity = 1;
+             document.getElementById("html_22_" + element[0]).style.opacity = 0.2;
+
+         } else {
+             document.getElementById("html_11_" + element[0]).style.opacity = 0.2;
+             document.getElementById("html_22_" + element[0]).style.opacity = 1;
+             html_mode_projet = "";
+
+         }
 
 
 
- ok.add("id_sha1_projet", element[0]); // ajout de l'information pour lenvoi 
- ok.add("html_mode_projet_1", html_mode_projet); // ajout de l'information pour lenvoi 
- console.log(ok.info()); // demande l'information dans le tableau
- ok.push(); // envoie l'information au code pkp  
+
+         var ok = new Information("config/html_mode_projet_2.php"); // création de la classe 
 
 
 
- 
+         ok.add("id_sha1_projet", element[0]); // ajout de l'information pour lenvoi 
+         ok.add("html_mode_projet_1", html_mode_projet); // ajout de l'information pour lenvoi 
+         console.log(ok.info()); // demande l'information dans le tableau
+         ok.push(); // envoie l'information au code pkp  
 
 
-    }
 
-       
+
+
+
+     }
  </script>
 
 
